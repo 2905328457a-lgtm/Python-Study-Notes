429fb4e7f8bc858d059a42a272f69aefd884ff25
YOLO <2905328457a@gmail.com>
2026-08-04 17:15:30 +0800

    Delete 02_核心语法 directory

--- 02_核心语法/08_异常/01_异常处理_案例.py
+++
@@ -1,8 +0,0 @@
-try:
-    print("==============================")
-    print(my)
-    print("******************************")
-except Exception as e: # 包含所有异常情况
-    print("程序出现错误，错误信息：", e)
-finally: # 可选
-    print("完毕")
